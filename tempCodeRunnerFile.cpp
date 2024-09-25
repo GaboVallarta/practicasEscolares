63860bc36a8dcf5f255a18d25c9ff95ebdde9422
GaboVallarta <gaboenvallarta@gmail.com>
2024-09-24 22:36:50 -0600

    reacomodo

--- tempCodeRunnerFile.cpp
+++
@@ -1,7 +0,0 @@
-for(int i=1;i<=a*h;i++)matriz.push_back(0);
-    // for(int i=1;i<=a;i++){
-    //     if(i%a==0)cout<<endl;
-    //     cout<<matriz[i]<<" ";
-    // }
-    // int x,y;
-    // cin>>x>>y;
